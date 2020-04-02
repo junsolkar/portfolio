d139a299a38387735816b3a51e30add32b129f41
Junaid Solkar <junaid-11@hotmail.co.uk>
2020-04-02 22:34:43 +0100

    navbar with arrow icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,34 @@
 import React from 'react'
-
+import {
+    AppBar,
+    Toolbar,
+    ListItem,
+    IconButton,
+    ListItemText,
+    Avatar,
+    Divider,
+    List,
+    Typography,
+    Box
+} from '@material-ui/core'
+import{
+    ArrowBack,
+    AssigntmentInd,
+    Home,
+    Apps,
+    ContactMail
+} from '@material-ui/icons'
 const Navbar = () => {
     return (
-        <div>
-            <h2>Nav bar</h2>
-        </div>
+        <Box component='nav'>
+           <AppBar position='static'style={{ background: '#222'}}>
+               <Toolbar>
+                   <IconButton>
+                        <ArrowBack style={{ color: 'tomato'}} />
+                   </IconButton>
+               </Toolbar>
+            </AppBar> 
+        </Box>
     )
 }
 
