7254e179c389c58dfbae4fc5326e22c4f2ec90bd
Junaid Solkar <junaid-11@hotmail.co.uk>
2020-04-07 18:46:46 +0100

    header useStlyes and positioning

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,21 +5,50 @@ import {
     Grid,
     Box
 } from '@material-ui/core'
+import {makeStyles} from '@material-ui/core/styles'
 import Typed from 'react-typed'
 import avatar from '../avatar.png'
 
+const useStyles = makeStyles(theme=>({
+    avatar:{
+        width:theme.spacing(15),
+        height:theme.spacing(15),
+        margin:theme.spacing(1),
+    },
+    title: {
+        color: "tomato"
+    },
+    subtitle: {
+        color: "tan",
+        marginBottom:"3rem"
+    },
+    typedContainer: {
+        position:"absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%,-50%)",
+        width:"100vw",
+        textAlign: "center",
+        zindex:1
+    }
+}));
+
 const Header = () => {
+    const classes = useStyles()
     return (
-        <Box>
-        <Avatar src={avatar} alt="junaid solkar"/>
-        <Typography variant="h4">
+        <Box className={classes.typedContainer}>
+        <Grid container justify="center">
+        <Avatar className={classes.avatar} src={avatar} alt="junaid solkar"/>
+        </Grid>
+        <Typography className={classes.title} variant="h4">
             <Typed strings={["Junaid Solkar"]} typeSpeed={40}/>
         </Typography>
         <br/>
-        <Typography variant="h5">
+        <Typography className={classes.subtitle} variant="h5">
             <Typed strings={["Mobile App Developer","Kotlin"]} typeSpeed={40}
             backSpeed={60}
-            loop/>
+            loop
+            />
         </Typography>
         </Box>
     );
