5449ba63efbd69c18c9e5ebfa1e231deab842251
Junaid Solkar <junaid-11@hotmail.co.uk>
2020-04-09 15:41:56 +0100

    added timeline to resume component

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,54 @@
 import React from 'react'
+import {makeStyles} from '@material-ui/core/styles'
+import {Box, Typography, Table} from '@material-ui/core'
+import Navbar from './Navbar'
 
+const useStyles = makeStyles(theme=>({
+    mainContainer:{
+        background: '#233'
+    },
+    timeline: {
+        position:'relative',
+        padding:'1rem',
+        margin:'0 auto',
+        "&:before": {
+            content:"''",
+            position:'absolute',
+            height:'100%',
+            border:'1px solid tan',
+            right:'40px',
+            top:0
+        },
+        "&:after":{
+            content:"''",
+            display:"table",
+            clear:'both'
+        },
+        [theme.breakpoints.up("md")]:{
+            padding:'2rem',
+            "&:before":{
+                left:'calc(50% - 1px)',
+                right:'auto'
+            }
+        }
+    }
+}));
 const Resume = () => {
+    const classes = useStyles()
     return (
-        <div>
-            
-        </div>
+        <>
+        <Navbar />
+        <Box component='header' className={classes.mainContainer}>
+            <Typography variant='h4' align='center'>
+                Work Experience
+            </Typography>
+            <Box component='div'className={classes.timeline}>
+            <Typography variant='h2'>
+                2019
+            </Typography>
+        </Box>
+        </Box>
+        </>
     )
 }
 
