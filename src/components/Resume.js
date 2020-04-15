715259783692dae560799111f09daeae6bb82a82
Junaid Solkar <junaid-11@hotmail.co.uk>
2020-04-15 16:41:02 +0100

    complete timeline

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -32,6 +32,41 @@ const useStyles = makeStyles(theme=>({
             }
         }
     },
+    timelineItem:{
+        padding: "1rem",
+        borderBottom:"2px solid tan",
+        position:"relative",
+        margin:"1rem 3rem 1rem 1rem",
+        clear: "both",
+        "&:before":{
+            content:"''",
+            position:"absolute",
+            right:"-0.625rem",
+            top:"calc(50% - 5px)",
+            borderStlye:"solid",
+            borderColor:"tomato tomato transparent transparent",
+            borderWidth:"0.625rem",
+            transform:"rotate(45deg)",
+        },
+        [theme.breakpoints.up("md")]:{
+            width:'44%',
+            margin:'1rem',
+            "&:nth-of-type(2n)": {
+                float:'right',
+                margin:'1rem',
+                borderColor:'tan'
+            },
+            "&:nth-of-type(2n):before": {
+                right:'auto',
+                left:"-0.625rem",
+                borderColor:"transparent transparent tomato tomato"
+            },
+        },
+        "&:after":{
+            content:"''",
+            position:"absolute",
+        },
+    },
     timelineYear:{
         textAlign:'center',
         maxWidth:'9.375rem',
@@ -55,6 +90,16 @@ const useStyles = makeStyles(theme=>({
                 display:'none'
             },
         }
+    },
+    heading :{
+        color:"tomato",
+        padding:"3rem 0",
+        textTransform: "uppercase"
+    },
+    subHeading:{
+        color:"white",
+        padding:"0",
+        textTransform: "uppercase"
     }
 }));
 const Resume = () => {
@@ -63,13 +108,52 @@ const Resume = () => {
         <>
         <Navbar />
         <Box component='header' className={classes.mainContainer}>
-            <Typography variant='h4' align='center'>
+            <Typography variant='h4' align='center' className={classes.heading}>
                 Work Experience
             </Typography>
             <Box component='div'className={classes.timeline}>
-            <Typography variant='h2'className={classes.timelineYear}>
+            <Typography variant='h2'className={`${classes.timelineYear} ${classes.timelineItem}`}>
                 2019
             </Typography>
+            <Box component='div' className={classes.timelineItem}>
+                <Typography variant='h5' align='center' className={classes.subHeading}>
+                    Lorem Ipsum
+                </Typography>
+                <Typography variant='body1' align='center'style={{color:"tomato"}}>
+                    Lorem Ipsum
+                </Typography>
+                <Typography variant='subtitle1' align='center' style={{color:"tan"}}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </Typography>
+            </Box>
+            <Typography variant='h2'className={`${classes.timelineYear} ${classes.timelineItem}`}>
+                2018
+            </Typography>
+            <Box component='div' className={classes.timelineItem}>
+                <Typography variant='h5' align='center' className={classes.subHeading}>
+                    Lorem Ipsum
+                </Typography>
+                <Typography variant='body1' align='center'style={{color:"tomato"}}>
+                    Lorem Ipsum
+                </Typography>
+                <Typography variant='subtitle1' align='center' style={{color:"tan"}}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </Typography>
+            </Box>
+            <Typography variant='h2'className={`${classes.timelineYear} ${classes.timelineItem}`}>
+                2017
+            </Typography>
+            <Box component='div' className={classes.timelineItem}>
+                <Typography variant='h5' align='center' className={classes.subHeading}>
+                    Lorem Ipsum
+                </Typography>
+                <Typography variant='body1' align='center'style={{color:"tomato"}}>
+                    Lorem Ipsum
+                </Typography>
+                <Typography variant='subtitle1' align='center' style={{color:"tan"}}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </Typography>
+            </Box>
         </Box>
         </Box>
         </>
